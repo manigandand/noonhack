daeed17c4263748c32a3133a46ba1a45e9a6b8c0
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-11-23 14:12:03 +0530

    client  push operation

--- client/file_server.go
+++ client/file_server.go
@@ -1,10 +1,12 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"noonhack/errors"
+	"noonhack/types"
 )
 
 type listQueueResponse struct {
@@ -18,8 +20,41 @@ type FileServerQueue struct {
 }
 
 // Push implements the QueueClient interface Push method
-func (f *FileServerQueue) Push(queueName string) {
+func (f *FileServerQueue) Push(queueName string, input interface{}) *errors.AppError {
+	url := fmt.Sprintf("%s/v1/queue/%s", f.client.ServerHost, queueName)
+	data := &types.QueueInput{
+		ServiceName: f.client.ServiceName,
+		Data:        input,
+	}
+	body, err := json.Marshal(data)
+	if err != nil {
+		return errors.BadRequest(err.Error())
+	}
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(body))
+	if err != nil {
+		return errors.InternalServer(err.Error())
+	}
 
+	client := http.DefaultClient
+	res, err := client.Do(req)
+	if err != nil {
+		return errors.InternalServer(err.Error())
+	}
+	defer res.Body.Close()
+
+	var response map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return errors.InternalServer(err.Error())
+	}
+
+	whathappened := response["data"]
+	if res.StatusCode > 299 {
+		whathappened = response["meta"]
+	}
+
+	fmt.Println(whathappened)
+
+	return nil
 }
 
 // Poll implements the QueueClient interface Poll method
